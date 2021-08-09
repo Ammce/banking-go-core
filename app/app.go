5508b8bd6690cb5fe4b3ad013ff1d8ced38b09d1
Amel Muminovic <amcenp@gmail.com>
2021-08-10 00:39:09 +0200

    Get Custoemr By ID

--- app/app.go
+++ app/app.go
@@ -11,6 +11,7 @@ func Start() {
 	router := mux.NewRouter()
 
 	router.HandleFunc("/customers", getAllCustomers).Methods(http.MethodGet)
+	router.HandleFunc("/customers/{customerId:[0-9]+}", getCustomerById).Methods(http.MethodGet)
 
 	log.Fatal(http.ListenAndServe("localhost:8080", router))
 }
--- app/handlers.go
+++ app/handlers.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 type Customer struct {
@@ -16,5 +18,13 @@ func getAllCustomers(w http.ResponseWriter, r *http.Request) {
 	customer := []Customer{{Name: "Amel", City: "Belgrade", Zipcode: 11000}}
 	w.Header().Add("Content-type", "application/json")
 	fmt.Println(r.Header.Get("Content-type"))
+	json.NewEncoder(w).Encode(&customer)
+}
+
+func getCustomerById(w http.ResponseWriter, r *http.Request) {
+	customerId := mux.Vars(r)["customerId"]
+	fmt.Println(customerId)
+	customer := Customer{Name: "Amel", City: "Belgrade", Zipcode: 11000}
+	w.Header().Add("Content-type", "application/json")
 	json.NewEncoder(w).Encode(&customer)
 }
