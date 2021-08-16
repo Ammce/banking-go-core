d0b888e30bfb19814cd4d185286d15ced74ae35a
Amel Muminovic <amcenp@gmail.com>
2021-08-16 09:45:48 +0200

    Repository created

---
+++ adapters/Account/accountRepositoryDB.go
@@ -0,0 +1,27 @@
+package accountAdapter
+
+import (
+	account "github.com/Ammce/go-banking-core/domain/Account"
+	"github.com/Ammce/go-banking-core/errs"
+	"github.com/Ammce/go-banking-core/logger"
+	"gorm.io/gorm"
+)
+
+type AccountRepositoryDB struct {
+	db *gorm.DB
+}
+
+func (ar AccountRepositoryDB) Save(a *account.Account) (*account.Account, *errs.AppError) {
+	result := ar.db.Create(&a)
+	if result.Error != nil {
+		logger.Error("Error while creating account - " + result.Error.Error())
+		return nil, errs.NewUnexpectedError("Error while creating account")
+	}
+
+	return a, nil
+
+}
+
+func NewAccountRepositoryDB(client *gorm.DB) AccountRepositoryDB {
+	return AccountRepositoryDB{db: client}
+}
